b2f29ada1ef96e8e357f2adfb56ef5d418a39161
Necnion8 <necnionch@gmail.com>
2023-06-23 16:17:26 +0900

    Add: --host, --port arguments

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-import logging
-
 from aiohttp import web
 
 from amuletbackend.app import create_server_app
 
 
 if __name__ == '__main__':
+    import logging
+    from argparse import ArgumentParser
+
     logging.basicConfig(level=logging.INFO, format="%(levelname)s - %(message)s")
-    web.run_app(create_server_app())
+
+    parser = ArgumentParser()
+    parser.add_argument("--host", default="0.0.0.0")
+    parser.add_argument("--port", default=8080, type=int)
+    args = parser.parse_args()
+
+    web.run_app(create_server_app(), host=args.host, port=args.port)
